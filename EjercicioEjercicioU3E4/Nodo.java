0cf6ae3fdb733c8703aff09f0a54ea8fba8525e8
Vianey Ambrosio <71795759+VianeyCA@users.noreply.github.com>
2020-11-28 19:43:05 -0600

    Create Nodo.java

---
+++ EjercicioEjercicioU3E4/Nodo.java
@@ -0,0 +1,13 @@
+package ejercicioejerciciou3e5;
+
+public class Nodo {
+
+    int Dato;
+    Nodo siguiente;
+
+    public Nodo(int D) {
+        this.Dato = D;
+        this.siguiente = null;
+    }
+    
+}
